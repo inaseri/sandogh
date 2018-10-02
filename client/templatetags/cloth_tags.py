cb820c04b4e7821ad8ac6cb228f3d8c961a9b994
mh.eftekhari <mh.eftekhari@gmail.com>
2018-10-02 13:10:33 +0330

    اصلاح سورس

--- client/templatetags/cloth_tags.py
+++ client/templatetags/cloth_tags.py
@@ -1,10 +1,10 @@
 # -*- coding: UTF-8 -*-
-from django import template
-from jdatetime import datetime as jdatetime
-from dateutil import tz
-register = template.Library()
-
-@register.filter("jdate")
-def jdate(value,format):
-    value = value.astimezone(tz.tzlocal())
-    return jdatetime.fromgregorian(datetime=value).strftime(format)
+# from django import template
+# from jdatetime import datetime as jdatetime
+# from dateutil import tz
+# register = template.Library()
+#
+# @register.filter("jdate")
+# def jdate(value,format):
+#     value = value.astimezone(tz.tzlocal())
+#     return jdatetime.fromgregorian(datetime=value).strftime(format)
