2e150a92492b6f526fc96e2de93ee6a3a34ba0fe
mh.eftekhari <mh.eftekhari@gmail.com>
2018-10-07 20:49:13 +0330

    اصلاح نام تابع متصل به آدرس تغییر گذرواژه

--- client/urls.py
+++ client/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index,name="home"),
     url(r'^accounts/login/', views.Login),
     url(r'^accounts/logout/', views.Logout, name="logout"),
-    url(r'^accounts/changepassword/', views.Logout,name="changepassword"),
+    url(r'^accounts/changepassword/', views.changepassword,name="changepassword"),
 #    url(r'^operator/(?P<page>[0-9a-z\.-]+)', views.operator,name='operator',),
 ]
