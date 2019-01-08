4107d5410f089931d7570353506520658fb6b7a6
mh eftekhari <mh.eftekhari@gmail.com>
2019-01-08 13:44:57 +0330

    تغییر در ساختار امتیاز دهی

--- client/management/commands/bankaccupdate.py
+++ client/management/commands/bankaccupdate.py
@@ -5,46 +5,50 @@ import datetime
 from bank.settings import BASE_DIR
 import os
 class Command(BaseCommand):
-    # calculate point backaccounts
-    file = open(os.path.join(BASE_DIR,"datetime.txt"),"r")
-    datetimeset = file.read()
-    file.close()
-    datetimeset = datetimeset.split("-")
-    datetimeset=datetime.date(int(datetimeset[0]),int(datetimeset[1]),int(datetimeset[2]))
-    acc =  bankaccount.objects.all()
-    for i in acc:
-        try:
-            loan = Loan_queue.objects.get(bankaccount=i,status=0)
-            loan = new_loan.objects.get(loan_queue = loan,status=0)
-            CountPayLoan = new_loan_pay.objects.filter(new_loan=loan,date__lte=datetimeset).count()
-            if loan.loan_queue.amount % loan.peak > 0:
-                CountAllLoan = loan.loan_queue.amount // loan.peak + 1
-            else:
-                CountAllLoan = loan.loan_queue.amount // loan.peak
-            last_day_date_time = datetimeset - datetime.timedelta(days=1)
-            if CountAllLoan == CountPayLoan:
-                Negative_point = 0
-            elif periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()<=CountPayLoan:
-                Negative_point = 0
-            else:
-                if periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()>CountAllLoan:
-                    Negative_point = CountAllLoan - CountPayLoan
+    def handle(self, *args, **options):
+        # calculate point backaccounts
+        file = open(os.path.join(BASE_DIR,"datetime.txt"),"r")
+        datetimeset = file.read()
+        file.close()
+        datetimeset = datetimeset.split("-")
+        if int(datetimeset[0]) == datetime.datetime.now().date().year and  int(datetimeset[1]) == datetime.datetime.now().date().month and int(datetimeset[2]) == datetime.datetime.now().date().day:
+            self.stdout.write('Your Database is Up To Date.')
+            return Flase
+        datetimeset=datetime.date(int(datetimeset[0]),int(datetimeset[1]),int(datetimeset[2]))
+        acc =  bankaccount.objects.all()
+        for i in acc:
+            try:
+                loan = Loan_queue.objects.get(bankaccount=i,status=0)
+                loan = new_loan.objects.get(loan_queue = loan,status=0)
+                CountPayLoan = new_loan_pay.objects.filter(new_loan=loan,date__lte=datetimeset).count()
+                if loan.loan_queue.amount % loan.peak > 0:
+                    CountAllLoan = loan.loan_queue.amount // loan.peak + 1
                 else:
-                    Negative_point = periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()-CountPayLoan
-            Negative_point = Negative_point * loan.peak // 10
-        except:
-            Negative_point=0
-        calsum = 0
-        colcatchsum = catch.objects.raw('SELECT id,price/10 as price FROM client_catch WHERE  bankaccount_id = '+str(i.id)+" and date <'"+str(datetimeset)+"' GROUP BY id")
-        for x in colcatchsum:
-            calsum +=x.price
-        i.points += int(calsum)
-        i.points -= Negative_point
-        i.save()
-        print(i.id,"updated.")
-    datetimeset = datetimeset + datetime.timedelta(days=1)
-    file = open(os.path.join(BASE_DIR, "datetime.txt"), "w")
-    file.write(str(datetimeset))
-    file.close()
-    print("date:" + str(datetimeset))
-    print('the End')
+                    CountAllLoan = loan.loan_queue.amount // loan.peak
+                last_day_date_time = datetimeset - datetime.timedelta(days=1)
+                if CountAllLoan == CountPayLoan:
+                    Negative_point = 0
+                elif periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()<=CountPayLoan:
+                    Negative_point = 0
+                else:
+                    if periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()>CountAllLoan:
+                        Negative_point = CountAllLoan - CountPayLoan
+                    else:
+                        Negative_point = periodic_payment.objects.filter(datetime__lte=last_day_date_time,datetime__gte=loan.date).count()-CountPayLoan
+                Negative_point = Negative_point * loan.peak // 10
+            except:
+                Negative_point=0
+            calsum = 0
+            colcatchsum = catch.objects.raw('SELECT id,price/10 as price FROM client_catch WHERE  bankaccount_id = '+str(i.id)+" and date <'"+str(datetimeset)+"' GROUP BY id")
+            for x in colcatchsum:
+                calsum +=x.price
+            i.points += int(calsum)
+            i.points -= Negative_point
+            i.save()
+            self.stdout.write(i.id,"updated.")
+        datetimeset = datetimeset + datetime.timedelta(days=1)
+        file = open(os.path.join(BASE_DIR, "datetime.txt"), "w")
+        file.write(str(datetimeset))
+        file.close()
+        self.stdout.write("date:" + str(datetimeset))
+        self.stdout.write('the End')
