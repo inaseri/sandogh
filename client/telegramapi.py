76fe85861086b5deda2b48c2f0137f156acfeae6
mh.eftekhari <mh.eftekhari@gmail.com>
2019-05-21 10:19:35 +0430

    اصلاح اسکریپت

--- client/telegramapi.py
+++ client/telegramapi.py
@@ -4,8 +4,6 @@ import requests,json
 def SendMessage(chat_id,text,reply_markup={}):
     url = "https://api.telegram.org/bot" + telegapiKey + "/"
     try:
-        return requests.post(url + "sendMessage",data={'chat_id':chat_id,"text":text,'reply_markup': json.dumps(reply_markup)},
-                    proxies=dict(http='socks5://sandogh:85mamad85@185.53.141.123:441',
-                                 https='socks5://sandogh:85mamad85@185.53.141.123:441'))
+        return requests.post(url + "sendMessage",data={'chat_id':chat_id,"text":text,'reply_markup': json.dumps(reply_markup)})
     except:
         return None
