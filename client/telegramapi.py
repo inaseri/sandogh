3f7559eaad9eb22ca5da9bcaa67c62d3c7670157
mh.eftekhari <mh.eftekhari@gmail.com>
2019-12-03 18:25:02 +0330

    set timeout for Telegram API to SendMessage

--- client/telegramapi.py
+++ client/telegramapi.py
@@ -4,6 +4,6 @@ import requests,json
 def SendMessage(chat_id,text,reply_markup={}):
     url = "https://api.telegram.org/bot" + telegapiKey + "/"
     try:
-        return requests.post(url + "sendMessage",data={'chat_id':chat_id,"text":text,'reply_markup': json.dumps(reply_markup)})
+        return requests.post(url + "sendMessage",data={'chat_id':chat_id,"text":text,'reply_markup': json.dumps(reply_markup)}, timeout=1.5)
     except:
         return None
